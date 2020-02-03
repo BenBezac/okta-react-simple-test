a04f3092beae4bbbce5d3704d53d38075e5887fd
BenBezac <benjamin.bezancon@gmail.com>
2020-02-03 10:00:15 +0100

    setting up okta key & url in .env.local

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route } from "react-router-dom";
-import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
-
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react'; 
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import Staff from "./components/pages/Staff";
 import Login from "./components/auth/Login";
-
+const API_URL = process.env.REACT_APP_OKTA_URL;
+const API_URL_FULL = `${API_URL}/oauth2/default`;
 
 function onAuthRequired({history}) {
     history.push('/login');
@@ -16,8 +16,8 @@ function onAuthRequired({history}) {
 function App() {
   return (
     <Router>
-        <Security issuer='XXXXXXX/oauth2/default'
-                  clientId='YYYYYYY'
+        <Security issuer={API_URL_FULL}
+                  clientId={process.env.REACT_APP_OKTA_API_KEY}
                   redirectUri={window.location.origin + '/implicit/callback'}
                   onAuthRequired={onAuthRequired}
                   pkce={true} >
@@ -26,7 +26,7 @@ function App() {
                 <div className="container">
                     <Route path="/" exact={true} component={Home}></Route>
                     <SecureRoute path='/Staff' component={Staff} />
-                    <Route path='/login' render={() => <Login baseUrl='XXXXXXX' />} />
+                    <Route path='/login' render={() => <Login baseUrl={API_URL} />} />
                     <Route path='/implicit/callback' component={ImplicitCallback} />
                 </div>
             </div>
